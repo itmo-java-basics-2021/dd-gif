252824d57f5d701df5b27b384f8a2992f2057191
dd-gif <ziqunion95@gmail.com>
2021-03-30 01:04:40 +0300

    Update DatabaseInputStream.java
    -lab 1

--- src/main/java/com/itmo/java/basics/logic/io/DatabaseInputStream.java
+++ src/main/java/com/itmo/java/basics/logic/io/DatabaseInputStream.java
@@ -38,7 +38,9 @@ public class DatabaseInputStream extends DataInputStream {
             return Optional.of(new RemoveDatabaseRecord(key));
         }
         byte[] value = in.readNBytes(valueSize);
-
+        if (value == null) {
+            return Optional.empty();
+        }
         return Optional.of(new SetDatabaseRecord(key, value));
     }
 }
