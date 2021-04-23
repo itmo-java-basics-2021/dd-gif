cd78f53624fbc860ed09917b68289668fae65ba2
sxedr <ziqunion95@gmail.com>
2021-04-23 18:02:25 +0300

    lab-2 -updated

--- src/main/java/com/itmo/java/basics/logic/impl/TableImpl.java
+++ src/main/java/com/itmo/java/basics/logic/impl/TableImpl.java
@@ -49,10 +49,7 @@ public class TableImpl implements Table {
     @Override
     public void write(String objectKey, byte[] objectValue) throws DatabaseException {
         try {
-            if (currentSegment == null) {
-                currentSegment = SegmentImpl.create(SegmentImpl.createSegmentName(tableName), path);
-                currentSegment.write(objectKey, objectValue);
-            } else if (!currentSegment.write(objectKey, objectValue)) {
+            if (!currentSegment.write(objectKey, objectValue)) {
                 currentSegment = SegmentImpl.create(SegmentImpl.createSegmentName(tableName), path);
                 currentSegment.write(objectKey, objectValue);
             }
